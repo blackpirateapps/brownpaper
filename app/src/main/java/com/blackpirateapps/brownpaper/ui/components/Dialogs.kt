0db5b835dd4fc1f0f331153a1afb1d202d031d77
blackpirateapps <github@blackpiratex.com>
2026-04-16 19:35:34 +0530

    Fix rememberSaveable imports in Compose UI

--- app/src/main/java/com/blackpirateapps/brownpaper/ui/components/Dialogs.kt
+++ app/src/main/java/com/blackpirateapps/brownpaper/ui/components/Dialogs.kt
@@ -20,8 +20,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -238,4 +238,3 @@ fun SearchInArticleDialog(
         },
     )
 }
-
